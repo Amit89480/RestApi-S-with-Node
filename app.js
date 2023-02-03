4e446355f1ee88926f32f13d7e3d3d7b54fcc45d
Amit Pandey <87856042+Amit89480@users.noreply.github.com>
2023-02-04 00:40:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port;
+const PORT = process.env.PORT || 5000;
 const product_route = require("./routes/product");
 const connectDB = require("./db/connect");
 
